b836772af0fdb1db6ab605d180006ae0f3a161dd
Jfranco2023 <u22311256@utp.edu.pe>
2024-12-09 18:09:23 -0500

    commit

--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -1,46 +1,21 @@
-// Espera a que el contenido del DOM esté completamente cargado antes de ejecutar el script
-document.addEventListener("DOMContentLoaded", function () {
-    // Selecciona el botón de usuario (imagen) y el menú desplegable
-    const userButton = document.querySelector(".user-img");
-    const dropdownMenu = document.querySelector(".dropdown-menu");
+const toggle = document.querySelector(".toggle")
+const menuDashboard = document.querySelector(".menu-dashboard")
+const iconoMenu = toggle.querySelector("i")
+const enlacesMenu = document.querySelectorAll(".enlace")
 
-    // Añade un evento de clic al botón de usuario
-    userButton.addEventListener("click", function (event) {
-        // Alterna la clase 'show' en el menú desplegable para mostrarlo u ocultarlo
-        dropdownMenu.classList.toggle("show");
-        // Evita que el evento de clic se propague a otros elementos
-        event.stopPropagation();
-    });
+toggle.addEventListener("click", () => {
+    menuDashboard.classList.toggle("open")
 
-    // Añade un evento de clic a la ventana (documento)
-    window.addEventListener("click", function () {
-        // Si el menú desplegable está visible, lo oculta removiendo la clase 'show'
-        if (dropdownMenu.classList.contains('show')) {
-            dropdownMenu.classList.remove('show');
-        }
-    });
-
-    // Añade un evento de clic al menú desplegable
-    dropdownMenu.addEventListener("click", function (event) {
-        // Evita que el evento de clic se propague a otros elementos
-        event.stopPropagation();
-    });
-});
-
-function redirigirSegunSesion(event) {
-    event.preventDefault(); // Evita la acción predeterminada del enlace
-
-    // Aquí tu lógica de redirección
-    var usuarioIniciado = /*[[${usuarioIniciado != null}]]*/ false;
-    var esAdmin = /*[[${usuarioIniciado?.isAdmin}]]*/ false;
-
-    if (usuarioIniciado) {
-        if (esAdmin) {
-            window.location.href = "/principalAdmin";
-        } else {
-            window.location.href = "/registroVentas";
-        }
-    } else {
-        window.location.href = "/index";
+    if(iconoMenu.classList.contains("bx-menu")){
+        iconoMenu.classList.replace("bx-menu", "bx-x")
+    }else {
+        iconoMenu.classList.replace("bx-x", "bx-menu")
     }
-}
+})
+
+enlacesMenu.forEach(enlace => {
+    enlace.addEventListener("click", () => {
+        menuDashboard.classList.add("open")
+        iconoMenu.classList.replace("bx-menu", "bx-x")
+    })
+})
